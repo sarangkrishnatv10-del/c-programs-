55d1fafb8ba08b9df6996e6340c7d1d5e43588eb
sarangkrishnatv10-del <sarangkrishnatv10@gmail.com>
2026-01-28 23:27:46 +0530

    Refactor Armstrong number checker implementation

--- 3_armstrong.c
+++ 3_armstrong.c
@@ -1,41 +1,25 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
 
-int countDigits(int num) {
-    int count = 0;
-    while (num > 0) {
-        count++;
-        num /= 10;
-    }
-    return count;
-}
+int main(){
+    int n, num, rem, result = 0;
 
-int isArmstrong(int num) {
-    int original = num;
-    int digits = countDigits(num);
-    int sum = 0;
-    
-    while (num > 0) {
-        int digit = num % 10;
-        sum += pow(digit, digits);
-        num /= 10;
-    }
-    
-    return original == sum;
-}
-
-int main() {
-    int num;
-    
-    printf("Armstrong Number Checker\n");
     printf("Enter a number: ");
-    scanf("%d", &num);
-    
-    if (isArmstrong(num)) {
-        printf("%d is an Armstrong number\n", num);
-    } else {
-        printf("%d is not an Armstrong number\n", num);
+    scanf("%d",&num);
+
+    n = num;
+    result = 0;
+
+    while(n != 0){
+        rem = n % 10;
+        result = result + (rem * rem * rem);
+        n = n / 10;
     }
-    
+
+    if(result == num){
+        printf("%d is an armstrong number\n", num);
+    } else {
+        printf("%d is not an armstrong number\n", num);
+    }
+
     return 0;
 }
