0682426776bb62001d777c6633616e0576691e30
sarangkrishnatv10-del <sarangkrishnatv10@gmail.com>
2026-01-28 23:55:03 +0530

    Update 4_array_reverse.c

--- 4_array_reverse.c
+++ 4_array_reverse.c
@@ -1,45 +1,19 @@
-#include <stdio.h>
-
-void reverseArray(int arr[], int size) {
-    printf("Original array: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    
-    int left = 0, right = size - 1;
-    
-    while (left < right) {
-        int temp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = temp;
-        left++;
-        right--;
-    }
-    
-    printf("Reversed array: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
-int main() {
-    int size;
-    
-    printf("Array Reverse Program\n");
-    printf("Enter the size of array: ");
-    scanf("%d", &size);
-    
-    int arr[size];
-    
-    printf("Enter %d elements:\n", size);
-    for (int i = 0; i < size; i++) {
-        printf("Element %d: ", i + 1);
-        scanf("%d", &arr[i]);
-    }
-    
-    reverseArray(arr, size);
-    
+#include<stdio.h> 
+#include<conio.h> 
+void main(){ 
+int a[100],i,n; 
+clrscr(); 
+printf("Enter the number of elements :"); 
+scanf("%d",&n); 
+printf("Enter the elements :"); 
+for(i=0;i<n;i++){ 
+scanf("%d",&a[i]); 
+} 
+printf("The elements in reverse : "); 
+for(i=n-1;i>=0;i--){ 
+printf("%d ",a[i]); 
+} 
+getch(); 
+} 
     return 0;
 }
