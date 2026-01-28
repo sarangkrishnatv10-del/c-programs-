c2bf08f243a566b7d69e616e932faeed0dfe599c
sarangkrishnatv10-del <sarangkrishnatv10@gmail.com>
2026-01-29 00:24:43 +0530

    Refactor 2D array program to improve structure

--- 5_largest_2d_array.c
+++ 5_largest_2d_array.c
@@ -1,34 +1,43 @@
-#include<stdio.h> 
-#include<conio.h> 
-void main() { 
-int i,j,r,c,a[100][100],lar=0; 
-clrscr(); 
-printf("Enter the number of rows :"); 
-scanf("%d",&r); 
-printf("enter the number of columns :"); 
-scanf("%d",&c); 
-printf("Enter each numbers :"); 
-for(i=0;i<=r;i++){ 
-for(j=0;j<=c;j++) { 
-printf("[%d][%d]",i,j); 
-scanf("%d",&a[i][j]); 
-} 
-} 
-for(i=0;i<=r;i++){ 
-for(j=0;j<=c;j++){ 
-printf("%d \t",a[i][j]); 
-} 
-printf("\n\n"); 
-} 
-for(i=0;i<=r;i++){ 
-for(j=0;j<=c;j++){ 
-if(a[i][j] > lar){ 
-lar= a[i][j]; 
-} 
-} 
-} 
-printf("The largest here is : %d",lar); 
-getch(); 
-}
+#include <stdio.h>
+
+int main() {
+    int i, j, r, c;
+    int a[100][100];
+    int lar;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &r);
+
+    printf("Enter the number of columns: ");
+    scanf("%d", &c);
+
+    printf("Enter the elements:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            printf("[%d][%d] = ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("\nMatrix elements are:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    lar = a[0][0];   // initialize largest value
+
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            if (a[i][j] > lar) {
+                lar = a[i][j];
+            }
+        }
+    }
+
+    printf("\nThe largest element is: %d\n", lar);
+
     return 0;
 }
